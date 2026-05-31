31519a000c7978872f287ff3f1957b5895450ec4
Bharath200415 <bharath200415@gmail.com>
2026-06-01 00:58:56 +0530

    implemented deployment service

--- uploadService/dist/aws.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const uploadFile: (fileName: string, localFilePath: string) => Promise<void>;
-//# sourceMappingURL=aws.d.ts.map
--- uploadService/dist/file.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const getAllFiles: (folderPath: string) => string[];
-//# sourceMappingURL=file.d.ts.map
