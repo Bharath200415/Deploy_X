b338b56be09baf9745e74a23bd46ce796a012075
Bharath200415 <bharath200415@gmail.com>
2026-06-01 01:17:10 +0530

    updates

---
+++ deployService/src/index.ts
@@ -0,0 +1,13 @@
+import { createClient } from 'redis';
+
+const subscriber = createClient();
+
+async function main(){
+    await subscriber.connect();
+    while (true){
+        const response = await subscriber.brPop('build-queue', 0);
+        console.log(response);
+    }
+}
+
+main().catch(console.error);
